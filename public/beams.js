20ca9373d77cdf07e63b4bb0bcd7b10d296d5065
Sanjit <sanjitmadan2006@gmail.com>
2026-08-10 15:21:44 +0400

    fix: change background canvases to fixed position to prevent double scrollbars

--- public/beams.js
+++ public/beams.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const canvas = document.createElement("canvas");
     canvas.id = "beams-canvas";
-    canvas.style.position = "absolute";
+    canvas.style.position = "fixed";
     canvas.style.top = "0";
     canvas.style.left = "0";
     canvas.style.width = "100%";
